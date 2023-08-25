62c860d7ae1ed3b332fa86048facb096de69c33e
Bhuvanesh Verma <workoholic24x7@gmail.com>
2023-08-25 18:25:15 +0200

    fix qa page

--- pages/interact.py
+++ pages/interact.py
@@ -7,6 +7,7 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Qdrant
+from st_pages import add_page_title
 
 
 def get_embedding_model():
@@ -56,34 +57,29 @@ def create_tab(data, db):
             st.write(f'Answer-{i+1}')
             st.write(doc.page_content)
 
-def main():
-    load_dotenv()
+load_dotenv()
 
-    st.set_page_config(page_title="Interact with Strandbox")
-    st.title("Interact with Strandbox")
+st.set_page_config(page_title="Interact with Strandbox")
+st.title("Interact with Strandbox")
 
-    uploaded_file = st.file_uploader("Choose a pdf file", accept_multiple_files=False)
-    if uploaded_file is not None:
-        data, vector_store = get_data(uploaded_file)
+uploaded_file = st.file_uploader("Choose a pdf file", accept_multiple_files=False)
+if uploaded_file is not None:
+    data, vector_store = get_data(uploaded_file)
 
-        tab1, tab2, tab3, tab4, tab5 = st.tabs(list(data.keys()))
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(list(data.keys()))
 
-        with tab1:
-            create_tab(data["Research Question"], vector_store)
+    with tab1:
+        create_tab(data["Research Question"], vector_store)
 
-        with tab2:
-            create_tab(data["Analytical Framework"], vector_store)
+    with tab2:
+        create_tab(data["Analytical Framework"], vector_store)
 
-        with tab3:
-            create_tab(data["Research Method"], vector_store)
+    with tab3:
+        create_tab(data["Research Method"], vector_store)
 
-        with tab4:
-            create_tab(data["Data Sources"], vector_store)
+    with tab4:
+        create_tab(data["Data Sources"], vector_store)
 
-        with tab5:
-            create_tab(data["Time Horizon"], vector_store)
+    with tab5:
+        create_tab(data["Time Horizon"], vector_store)
 
-
-
-if __name__ == '__main__':
-    main()
